258ee4156c08cc1327ade24c5049b490fb7339ff
Anas <anasansari2357@gmail.com>
2025-06-03 12:12:34 +0530

    Change default port for backend from 3000 to 8080

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const pinecone = initPinecone();
 
