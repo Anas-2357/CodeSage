06e0a35879f7028e830ca925363530f0a6e7918e
Anas <anasansari2357@gmail.com>
2025-06-08 11:07:50 +0530

    Allow cors for localhost

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,12 @@ const globalLimiter = rateLimit({
     },
 });
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(globalLimiter);
